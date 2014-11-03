694af39dd46df76c23231d0804e586bdb9dceb8e
maiertho <thomas.maier@hs-furtwangen.de>
2014-11-03 16:39:07 +0100

    erster Schuss

---
+++ SimplePay/SimplePay/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using SimplePay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimplePay.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        SimplePay_HaendlerdatenEntities db = new SimplePay_HaendlerdatenEntities();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            Tisch tisch = db.Tisch.Find(id);
+            return tisch.bezeichnung;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
