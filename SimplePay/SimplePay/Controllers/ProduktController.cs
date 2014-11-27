506b47837762adadc0f6dca2339e2556cb3ab05e
maiertho <thomas.maier@hs-furtwangen.de>
2014-11-27 10:20:26 +0100

    Sim-28_Produkt_Controller_Init_tm

--- SimplePay/SimplePay/Controllers/ProduktController.cs
+++ SimplePay/SimplePay/Controllers/ProduktController.cs
@@ -11,12 +11,15 @@ namespace SimplePay.Controllers
     public class ProduktController : ApiController
     {
         SimplePay_HaendlerdatenEntities db = new SimplePay_HaendlerdatenEntities();
+        
         // GET api/produkt
+        // Sim-28 Komplette Liste der Produkte
         public IEnumerable<Produkt> Get()
         {
             return db.Produkt.ToList();
         }
 
+        /*
         // GET api/produkt/5
         public IEnumerable<Produkt> Get(int id)
         {
@@ -29,26 +32,8 @@ namespace SimplePay.Controllers
             //{
             //    int produkt_kategorie = produkt.p_id;
             //    db.Produkt_Kategorie.
-            //}
-            
-
-            
+            //}   
         }
-
-
-        // POST api/produkt
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/produkt/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/produkt/5
-        public void Delete(int id)
-        {
-        }
+        */
     }
 }
