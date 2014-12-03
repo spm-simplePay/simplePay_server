15df19462b06b1bb3e2e8d467c24be9fbc4f8391
maiertho <thomas.maier@hs-furtwangen.de>
2014-12-03 17:42:58 +0100

    sim-57_test_Produktabfrage

--- SimplePay/SimplePay.Tests/UnitTest2.cs
+++ SimplePay/SimplePay.Tests/UnitTest2.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplePay.Models;
+using SimplePay.Controllers;
+using System.Collections.Generic;
+using System.Transactions;
 
 namespace SimplePay.Tests
 {
     [TestClass]
     public class UnitTest2
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        TransactionScope _trans;
+
+        /*
+         * Wird vor dem Tests aufgeruden, so dass die DAten nicht dauerhaft in die DB geschrieben werden
+         */
+        [TestInitialize()]
+        public void Init()
         {
+            _trans = new TransactionScope();
+
+        }
+
+        /*
+         * Wird nach dem Test aufgerufen, so dass die Daten nicht dauerhaft in die DB geschrieben werden
+         */
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _trans.Dispose();
+        }
+
+
+
+        /*
+        *Diese Testmethode prüft die Abfrage des Produktangebots
+        */
+        [TestMethod]
+        public void Get_Produkt_Menu()
+        {
+            ProduktController produktController = new ProduktController();
+            IEnumerable <Produkt> produkte = produktController.Get();
+            if (produkte == null)
+            {
+                throw new Exception();
+            }
+
+
+
         }
     }
 }
